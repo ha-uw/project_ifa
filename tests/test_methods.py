0f7fba324e7c7e472cf34785c3c207a020f18836
raulcpoliveira <raulcpoliveira@gmail.com>
2024-07-01 23:37:11 +0100

    Refactor code for improved modularity and readability

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,10 +1,8 @@
 import unittest
-from unittest.mock import patch
-
+from pathlib import Path
 from src.methods.deepdta import DeepDTA
 from src.methods.graphdta import GraphDTA
 from src.methods.widedta import WideDTA
-from pathlib import Path
 
 from torch import set_float32_matmul_precision
 
@@ -17,7 +15,7 @@ class TestMethods(unittest.TestCase):
         deepdta = DeepDTA(config_file, fast_dev_run=True)
 
         try:
-            deepdta.train()
+            deepdta.run_k_fold_validation(2)
         except Exception as e:
             self.fail(f"deepdta function failed with an exception: {e}")
 
@@ -27,7 +25,7 @@ class TestMethods(unittest.TestCase):
         graphdta = GraphDTA(config_file, drug_encoder=drug_encoder, fast_dev_run=True)
 
         try:
-            graphdta.train()
+            graphdta.run_k_fold_validation(2)
         except Exception as e:
             self.fail(f"graphdta function failed with an exception: {e}")
 
@@ -36,7 +34,7 @@ class TestMethods(unittest.TestCase):
         widedta = WideDTA(config_file, fast_dev_run=True)
 
         try:
-            widedta.run_k_fold_validation(5)
+            widedta.run_k_fold_validation(2)
         except Exception as e:
             self.fail(f"widedta function failed with an exception: {e}")
 
